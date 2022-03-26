7124cfe5e96e69b4dfe29aedd7580424a3f3aada
SomehowPatrick <somehowpatrick@protonmail.com>
2022-03-26 15:53:51 +0100

    Project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+import Test from './Test';
+
+function App() {
+  return (
+    <div>
+      <Test name="Günter"/>
+      <p class="text-4xl text-red-400 tracking-widest"> Hello there</p>
+    </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/Test.jsx
@@ -0,0 +1,7 @@
+function Test(props) {
+    return(
+        <h1>Hello {props.name}</h1>
+    );
+}
+
+export default Test;
